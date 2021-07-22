cd801fa57e42d6bec0089d2ec981874597e84cec
SteveHicksCH <shicks@companieshouse.gov.uk>
2021-07-22 16:56:54 +0100

    create environment related metric

--- groups/stack/module-cloudwatch/dashboard.tf
+++ groups/stack/module-cloudwatch/dashboard.tf
@@ -1,3 +1,19 @@
+resource "aws_cloudwatch_log_metric_filter" "requests-on-queue_metric_filter" {
+  name           = "requests-on-queue-${var.environment}"
+  pattern        = "{ ($.data.log_record_name = \"ocr_statistics\") && ($.data.queue_size > 0) }"
+  log_group_name = "/ecs/ocr-api-${var.environment}/ocr-api"
+
+ # Support for unit attribute in Add support for unit 3.47.0 (June 24, 2021) https://github.com/hashicorp/terraform-provider-aws/blob/main/CHANGELOG.md
+ # ideally set: unit = "Count"
+  metric_transformation {
+    name          = "queue-size-above-zero"
+    namespace     = "ocr-api-metrics-${var.environment}"
+    value         = "$.data.queue_size"
+    default_value = 0
+  }
+}
+
+
 resource "aws_cloudwatch_dashboard" "ocr_api_dashboard" {
   dashboard_name = "ocr-api-${var.environment}"
 
@@ -8,3 +24,4 @@ resource "aws_cloudwatch_dashboard" "ocr_api_dashboard" {
   })
 
 }
+
